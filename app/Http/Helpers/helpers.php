a306e4705d8a41eb865ac7095ad3694f7a7ce74a
shaon99 <shaonahmed703@gmail.com>
2024-10-13 16:09:14 +0600

    fe

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -16,8 +16,40 @@ function imageUploadWithoutCrop($file, $folderName, $oldFile)
     }
     $baseUrl = env('APP_URL');
 
-    $fileName = $baseUrl.'/'.filePath($folderName).'/'.uniqid() . '.' . $file->getClientOriginalExtension();
+    $fileName = $baseUrl . '/' . filePath($folderName) . '/' . uniqid() . '.' . $file->getClientOriginalExtension();
     $file->move(filePath($folderName), $fileName);
 
     return $fileName;
+}
+
+
+//cloudinary upload
+function cloudUpload($image, $folder, $old)
+{
+    preg_replace('/\.[^.]+$/', '.', $image->getClientOriginalName()) . 'webp';
+
+    if ($old) {
+        $token = explode('/', $old);
+        $token2 = explode('.', $token[sizeof($token) - 1]);
+        cloudinary()->destroy('ecom/' . $folder . '/' . $token2[0]);
+    }
+    $response = cloudinary()->upload($image->getRealPath(), [
+        'folder' => 'ecom/' . $folder,
+        'transformation' => [
+            'crop' => 'fill',
+            'quality' => 'auto:best',
+            'fetch_format' => 'auto',
+        ]
+    ])->getSecurePath();
+
+    return $response;
+}
+
+// //remove image from cloud
+function imageRemoveFromCloud($image, $folder)
+{
+    $token = explode('/', $image);
+    $token2 = explode('.', $token[sizeof($token) - 1]);
+    $response = cloudinary()->destroy('ecom/' . $folder . '/' . $token2[0]);
+    return $response;
 }
